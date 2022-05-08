78d2dfde0a2643c46288109224176c7b4e6cc6ef
kraxarn <kraxie@protonmail.com>
2022-05-08 20:33:33 +0200

    Some initial logic

--- src/main/kotlin/com/kraxarn/dbselector/MainWindowFactory.kt
+++ src/main/kotlin/com/kraxarn/dbselector/MainWindowFactory.kt
@@ -10,7 +10,7 @@ class MainWindowFactory : ToolWindowFactory
 {
 	override fun createToolWindowContent(project: Project, toolWindow: ToolWindow)
 	{
-		val mainForm = MainFormLogic()
+		val mainForm = MainFormLogic(project)
 		val contentFactory = ContentFactory.SERVICE.getInstance()
 		val content = contentFactory.createContent(mainForm.content, "", false)
 		toolWindow.contentManager.addContent(content)
--- src/main/kotlin/com/kraxarn/dbselector/ui/MainFormLogic.kt
+++ src/main/kotlin/com/kraxarn/dbselector/ui/MainFormLogic.kt
@@ -1,9 +1,47 @@
 package com.kraxarn.dbselector.ui
 
+import com.intellij.openapi.project.Project
+import com.kraxarn.dbselector.extensions.notifyError
+import java.awt.Desktop
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import java.net.URI
+import javax.swing.AbstractAction
 import javax.swing.JPanel
+import kotlin.random.Random
 
-class MainFormLogic : MainForm()
+class MainFormLogic(val project: Project) : MainForm()
 {
+	init
+	{
+		// Server
+		comboBoxServer.addItem("localhost")
+
+		// Databases
+
+		// Scripts
+
+		// Passwords
+
+		//region About
+
+		buttonAboutOpenSource.addActionListener {
+			try
+			{
+				val desktop = Desktop.getDesktop()
+				val uri = URI("https://github.com/kraxarn/db-selector")
+				desktop.browse(uri)
+			} catch (e: Exception)
+			{
+				project.notifyError("Failed to open link", e.message ?: "Unknown error")
+			}
+		}
+
+		labelAboutSecret.isVisible = Random.Default.nextInt() < Int.MAX_VALUE / 8
+
+		//endregion
+	}
+
 	val content: JPanel
 		get() = panelMain
 }
