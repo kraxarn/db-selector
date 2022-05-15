49138e00d6f6d9ed99c86ce3a4e7053ec3667612
kraxarn <kraxie@protonmail.com>
2022-05-15 18:16:12 +0200

    Initial database logic

---
+++ src/main/kotlin/com/kraxarn/dbselector/Database.kt
@@ -0,0 +1,27 @@
+package com.kraxarn.dbselector
+
+import java.sql.DriverManager
+
+class Database(private val hostname: String, private val databaseName: String?)
+{
+	private val connection = DriverManager.getConnection(connectionUrl)
+
+	private val connectionUrl: String
+		get() = "jdbc:sqlserver://$hostname;databaseName=${databaseName ?: "master"}"
+
+	val databaseNames: List<String>
+		get()
+		{
+			val sql = "select * from sys.databases where has_perms_by_name(name, 'database', 'create_table') = 1";
+			val results = mutableListOf<String>()
+
+			val stmt = connection.createStatement()
+			val result = stmt.executeQuery(sql)
+			while (result.next())
+			{
+				results.add(result.getString(0))
+			}
+
+			return results
+		}
+}
